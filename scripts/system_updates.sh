87c8fdeca84af81be34280d1f75e07d7f0dfde3c
Animesh mishra <142673038+aniismess@users.noreply.github.com>
2025-07-24 07:46:05 +0000

    Update system_updates.sh

--- scripts/system_updates.sh
+++ scripts/system_updates.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 
 # made by ani
-# This script checks for system updates. I like to keep my system fresh and secure, so I always know when it's time to update.
+# This script checks for system updates.
 
-# I use `checkupdates` to see how many packages need an upgrade. It's like checking for new loot!
+# I use `checkupdates` to see how many packages need an upgrade. 
 UPDATES=$(checkupdates 2>/dev/null | wc -l)
 
-# If there are no updates, I show a green checkmark and a zero. All clear!
+# If there are no updates, I show a green checkmark and a zero.
 if [ "$UPDATES" -eq 0 ]; then
     echo " 0"
-# Otherwise, I show a warning icon and the number of pending updates. Time to get to work!
 else
     echo " ${UPDATES}"
 fi
